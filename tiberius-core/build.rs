7eb7a300303ea7bec13608c2a4689c7900e0df36
cultpony <67918945+cultpony@users.noreply.github.com>
2021-08-14 22:15:47 +0200

    stabilize core build script

--- tiberius-core/build.rs
+++ tiberius-core/build.rs
@@ -1,33 +1,35 @@
-use std::convert::TryFrom;
-use std::process::Command;
 use change_detection::ChangeDetection;
+use std::convert::TryFrom;
+use std::path::Path;
+use std::process::Command;
 
 fn main() {
     let debug = std::env::var("PROFILE").unwrap() != "release";
-    if debug {
+    if !debug {
         let assetdir = "../res/assets-build";
         let assetdir = std::path::PathBuf::try_from(assetdir).unwrap();
         let assetdir = assetdir.canonicalize().unwrap();
-        println!("cargo:warning=Deleting old asset build dir first: {}", assetdir.display());
+        println!(
+            "cargo:warning=Deleting old asset build dir first: {}",
+            assetdir.display()
+        );
         std::fs::remove_dir_all(assetdir).unwrap();
     }
     let builddir = "../res/assets";
     let builddir = std::path::PathBuf::try_from(builddir).unwrap();
     let builddir = builddir.canonicalize().unwrap();
-    ChangeDetection::path(format!("{}", builddir.display()))
-        .generate();
+    ChangeDetection::path_exclude("../res/assets", |x: &Path| {
+        x.starts_with("../res/assets/node_modules")
+    })
+    .generate();
     println!("cargo:warning=Building in {}", builddir.display());
     let mut cmd = Command::new("yarn");
     let out = {
         if debug {
-            cmd
-                .arg("devbuild")
-                .env("NODE_ENV", "development")
+            cmd.arg("devbuild").env("NODE_ENV", "development")
         } else {
             println!("cargo:warning=Production Asset Build");
-            cmd
-                .arg("deploy")
-                .env("NODE_ENV", "production")
+            cmd.arg("deploy").env("NODE_ENV", "production")
         }
     }
     .current_dir(builddir)
