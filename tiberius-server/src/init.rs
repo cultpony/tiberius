034b724f2bdfbbe8bb7e8b85b6ccd7fa798015e5
cultpony <67918945+cultpony@users.noreply.github.com>
2021-12-11 20:02:41 +0100

    unclamp logger

--- tiberius-server/src/init.rs
+++ tiberius-server/src/init.rs
@@ -8,14 +8,16 @@ lazy_static! {
         if let Err(e) = kankyo::load(false) {
             info!("couldn't load .env file: {}, this is probably fine", e);
         }
-        flexi_logger::Logger::with(
+        /*flexi_logger::Logger::with(
             flexi_logger::LogSpecification::builder()
                 .module("sqlx", LevelFilter::Warn)
                 .module("sqlx::query", LevelFilter::Warn)
                 .module("sqlxmq", LevelFilter::Warn)
                 .module("tiberius_search", LevelFilter::Warn)
                 .build(),
-        )
+        )*/
+        flexi_logger::Logger::try_with_env()
+        .unwrap()
         .start()
         .unwrap()
     };
