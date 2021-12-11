c8964d042fbf1fa0f6f684a5fcf309d072d3ae13
cultpony <67918945+cultpony@users.noreply.github.com>
2021-12-11 20:10:09 +0100

    use env for log level

--- tiberius-server/src/init.rs
+++ tiberius-server/src/init.rs
@@ -1,6 +1,7 @@
 use flexi_logger::{LevelFilter, LoggerHandle};
 use lazy_static::lazy_static;
 use tracing::info;
+use std::str::FromStr;
 
 lazy_static! {
     pub static ref LOGGER: LoggerHandle = {
@@ -8,16 +9,17 @@ lazy_static! {
         if let Err(e) = kankyo::load(false) {
             info!("couldn't load .env file: {}, this is probably fine", e);
         }
-        /*flexi_logger::Logger::with(
+        let def_level = std::env::var("RUST_LOG").unwrap();
+        let def_level = LevelFilter::from_str(&def_level).unwrap();
+        flexi_logger::Logger::with(
             flexi_logger::LogSpecification::builder()
+                .default(def_level)
                 .module("sqlx", LevelFilter::Warn)
                 .module("sqlx::query", LevelFilter::Warn)
                 .module("sqlxmq", LevelFilter::Warn)
                 .module("tiberius_search", LevelFilter::Warn)
                 .build(),
-        )*/
-        flexi_logger::Logger::try_with_env()
-        .unwrap()
+        )
         .start()
         .unwrap()
     };
